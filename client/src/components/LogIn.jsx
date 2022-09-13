a3eae0b9ec73ea075597d577131ff8888715fcf0
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-09-13 14:35:38 +0500

    Update LogIn.jsx

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -4,6 +4,17 @@ function LogIn() {
   return (
     <div className='container'>
 <div className="row justify-content-center mt-4">
+  <div className='col-lg-5 card border-primary mt-4'>
+  <div className="card-header">Header</div>
+  <div className="card-body">
+    <h4 className="card-title">Primary card title</h4>
+    <p className="card-text">
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </p>
+  </div>
+
+  </div>
 
 </div>
     </div>
