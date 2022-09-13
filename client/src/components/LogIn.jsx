f343a9c722ab48e12488d11aa17665648feee244
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-09-13 14:47:28 +0500

    Login UI complete

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -2,22 +2,31 @@ import React from 'react'
 
 function LogIn() {
   return (
-    <div className='container'>
-<div className="row justify-content-center mt-4">
-  <div className='col-lg-5 card border-primary mt-4'>
-  <div className="card-header">Header</div>
-  <div className="card-body">
-    <h4 className="card-title">Primary card title</h4>
-    <p className="card-text">
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </p>
-  </div>
-
-  </div>
-
-</div>
+  <div className='container'>
+    <div className="row justify-content-center mt-4">
+    <div className='col-lg-5 card border-primary mt-4'>
+       
+        <div className="card-body">
+           <h4 className="card-title">LogIn Now</h4>
+           <div class="form-group">
+      <label for="exampleInputEmail1" class="form-label mt-4">Email or Username</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email or username"/>
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
+
+    <div class="form-group">
+      <label for="exampleInputEmail1" class="form-label mt-4">Password</label>
+      <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Password"/>
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+
+    <button type="button" class="btn btn-primary">Login</button>
+        </div>
+
+    </div>
+
+     </div>
+</div>
   )
 }
 
